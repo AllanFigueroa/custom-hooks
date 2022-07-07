ce59efe90e7d45d1be5dbec936568a705c140d2c
Allan Figueroa <allanfigueroa@outlook.com>
2022-07-07 12:37:02 +0200

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const init = () =>{
+    return JSON.parse(localStorage.getItem("todos")) || [];
+}
+
+export const useTodo = (initialState = []) =>
+{
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    //Graba en todo momento en el LocalStorage
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+    }, [todos])
+    
+    const handleNewTodo = (todo) => {
+        // console.log(todo);
+        const action = {
+            type : "[TODO] add todo",
+            payload : todo,
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) =>{
+        dispatch({
+            type: '[TODO] remove todo',
+            payload: id,
+        })
+    }
+
+    const handleToggleTodo = (id) =>{
+        dispatch({
+            type : '[TODO] toggle todo',
+            payload: id,
+        })
+    }
+    return {
+        todos,
+        todosCount : todos.length,
+        PendingTodosCount : todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
